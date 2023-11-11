6bebb5fedd33b8f3830ca33d101a366c2f8b1c8c
welli7ngton <wellingtonasilva45@gmail.com>
2023-11-11 10:49:32 -0300

    solução: exercício quadrantes

--- estruturas-condicionais/coordenadas.java
+++ estruturas-condicionais/coordenadas.java
@@ -12,10 +12,28 @@ public class coordenadas {
     public static void main(String[] args){
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        System.out.println("Digite a coordenada x: ")
+        System.out.println("Digite a coordenada x: ");
         int x = sc.nextInt();
-        System.out.println("Digite a coordenada y: ")
+        System.out.println("Digite a coordenada y: ");
         int y = sc.nextInt();
+
+        if (x == 0 && y == 0){
+            System.out.println("Origem");
+        } else if (x < 0 && y > 0){
+            System.out.println("Q2");
+        } else if (x > 0 && y > 0){
+            System.out.println("Q1");
+        } else if (x > 0 && y < 0){
+            System.out.println("Q4");
+        } else if (x < 0 && y < 0){
+            System.out.println("Q3");
+        } else if (x == 0 && y != 0) {
+            System.out.println("Eixo y");
+        } else if (x != 0 && y == 0) {
+            System.out.println("Eixo x");
+        }
+
+
         sc.close();
     }
 }
